8992790efd46bbfc1423e36fb67a27871376cffb
Rohit <rahulyoyorawat@gmail.com>
2020-04-21 15:17:35 +0530

    controller modified

--- controller/gestController.js
+++ controller/gestController.js
@@ -20,25 +20,103 @@ exports.getClubs = (req,res,next) => {
      
 }
 
-
-exports.getIndex = (req,res,next) => {
-
+function checKAdmin(req){
   var isAdmin = false;
 
   if (req.session.isLoggedIn) {
     if(req.session.user.isAdmin){
-       isAdmin = true;
-      }
+      isAdmin = true;
     }
-  
+  }
+
+  return isAdmin;
+
+
+} 
+ 
+
+
+exports.getIndex = (req,res,next) => {
 
   Clubs.find({}).limit(3).exec( (err, clubs) => {
     res.render('index',{
       isAuthenticated: req.session.isLoggedIn,
-      isAdmin : isAdmin,
-      clubs : clubs
+      isAdmin : checKAdmin(req),
+      clubs : clubs,
+      userType : req.session.user.userType
     
     });
     
   })
+}
+
+
+exports.getNotice = (req,res,next) => {
+  res.render('notice',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getNoticeSingle = (req,res,next) => {
+  res.render('notice-single',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getEventSingle = (req,res,next) => {
+  res.render('event-single',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+
+exports.getEvents = (req,res,next) => {
+  res.render('events',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getContact = (req,res,next) => {
+  res.render('contact',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getClubsSingle = (req,res,next) => {
+  res.render('club-single',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getAbout = (req,res,next) => {
+  res.render('about',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType
+  });
+}
+
+exports.getProfile = (req,res,next) => {
+
+  res.render('profile',{
+    isAuthenticated: req.session.isLoggedIn,
+    isAdmin : checKAdmin(req),
+    userType : req.session.user.userType,
+    nameFirst:req.session.user.profile.firstName,
+    nameLast : req.session.user.profile.lastName,
+  });
+  
+  
 }
--- routs/gest.js
+++ routs/gest.js
@@ -8,105 +8,19 @@ router.post('/login',auth.getlogindetails);
 
  router.post('/signup',auth.getSignUpDetails);
  
-router.get('/profile',isAuth,(req,res,next) => {
-  console.log(req.session.user.profile.firstName);
+router.get('/profile',isAuth,GustController.getProfile);
 
-  if(req.session.user.isAdmin){
-    res.render('admin-profile',{
-      isAuthenticated: req.session.isLoggedIn,
-      nameFirst:req.session.user.profile.firstName,
-        nameLast : req.session.user.profile.lastName,
-    });
-  }else{
-    res.render('profile',{
-      isAuthenticated: req.session.isLoggedIn,
-      nameFirst:req.session.user.profile.firstName,
-        nameLast : req.session.user.profile.lastName,
-    
-    });
-  }
-  
-});
-
-router.get('/create-club',isAuth,(req,res,next) => {
-  console.log(req.session.user.profile.firstName);
-    res.render('profile',{
-    
-        isAdmin:  req.session.user.isAdmin,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        },
-        nameFirst:req.session.user.profile.firstName,
-        nameLast : req.session.user.profile.lastName
-        
-      });
-});
 
 router.get('/clubs',GustController.getClubs);
 
-router.get('/about',(req,res,next) => {
-    res.render('about',{
-      
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
- router.get('/club-single',(req,res,next) => {
-    res.render('club-single',{
-      
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
-router.get('/contact',(req,res,next) => {
-    res.render('contact',{
-     
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
-router.get('/events',(req,res,next) => {
-    res.render('events',{
-      
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
- router.get('/event-single',(req,res,next) => {
-    res.render('event-single',{
-      
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
-router.get('/notice-single',(req,res,next) => {
-    res.render('notice-single',{
-      
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
-router.get('/notice',(req,res,next) => {
-    res.render('notice',{
-      isAdmin:  req.session.user.isAdmin,
-        isAuthenticated: req.session.isLoggedIn,
-        if (isAuthenticated) {
-          isAdmin:  req.session.user.isAdmin
-        }
-      });
-})
+router.get('/about',GustController.getAbout)
+ router.get('/club-single',GustController.getClubsSingle)
+router.get('/contact',GustController.getContact)
+
+router.get('/events',GustController.getEvents)
+ router.get('/event-single',GustController.getEventSingle)
+router.get('/notice-single',GustController.getNoticeSingle)
+router.get('/notice',GustController.getNotice)
 
  router.get('/index',GustController.getIndex)
 
